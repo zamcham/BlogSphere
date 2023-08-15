a469cf6903ce65fd0f3acdb11c21439389a7bffb
Cristian Zambrano <criszamcham@gmail.com>
2023-08-15 16:48:24 -0400

    Add comment count method and rspec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,10 @@ class Comment < ApplicationRecord
   validates :user_id, presence: true
   validates :post_id, presence: true
   validates :content, presence: true, length: { maximum: 1000 }
+
+  after_create :increment_post_comment_count
+
+  def increment_post_comment_count
+    post.increment!(:comment_count)
+  end
 end
