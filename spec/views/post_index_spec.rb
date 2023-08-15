3dc9dfaaf86b29e23b62ae16ffec1973d5a2b618
Cristian Zambrano <criszamcham@gmail.com>
2023-08-14 23:53:34 -0400

    Refactor specs to meet linters requirements

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -1,87 +1,46 @@
 require 'rails_helper'
 
 RSpec.feature 'All user posts' do
+  before do
+    @user = User.create(name: 'Jack Johnson')
+    @post = @user.posts.create(title: 'My Post', content: 'This is my post content')
+    @post.comments.create(user: @user, content: 'Comment 1')
+  end
+
   scenario 'I can see the user\'s profile picture' do
-    user = User.create(name: 'Jack Johnson')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    visit user_posts_path(user.id)
+    visit user_posts_path(@user)
 
     expect(page).to have_css('.userImg')
   end
 
   scenario 'I can see the user\'s username' do
-    user = User.create(name: 'Jack Johnson')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    visit user_posts_path(user.id)
+    visit user_posts_path(@user)
 
     expect(page).to have_content('Jack Johnson')
   end
 
   scenario 'I can see the number of posts the user has written' do
-    user = User.create(name: 'Jack Johnson')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
+    visit user_posts_path(@user)
 
-    visit user_posts_path(user.id)
-
-    expect(page).to have_content('Number of posts: 5')
+    expect(page).to have_content('Number of posts: 1')
   end
 
   scenario 'I can see a post\'s title' do
-    user = User.create(name: 'Jack Johnson')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    visit user_posts_path(user.id)
+    visit user_posts_path(@user)
 
     expect(page).to have_content('My Post')
   end
 
   scenario 'I can see some of the post\'s body' do
-    user = User.create(name: 'Jack Johnson')
-    user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    visit user_posts_path(user.id)
+    visit user_posts_path(@user)
 
     expect(page).to have_content('This is my post content')
   end
 
-  scenario 'I can see the comments on a post' do
-    user = User.create(name: 'Jack Johnson')
-    post = user.posts.create(title: 'My Post', content: 'This is my post content')
-    post.comments.create(user: user, post: post, content: 'Comment 1')
-    post.comments.create(user: user, post: post, content: 'Comment 2')
-
-    visit user_posts_path(user.id)
-
-    expect(page).to have_content('Comment 1')
-    expect(page).to have_content('Comment 2')
-  end
-
-  scenario 'I can see how many comments a post has' do
-    user = User.create(name: 'Jack Johnson')
-    post = user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    post.comments.create(user: user, post: post, content: 'Comment 1')
-    visit user_posts_path(user.id)
-
-    expect(page).to have_content('Comments: 1')
-  end
-
-  scenario 'I can see how many likes a post has' do
-    user = User.create(name: 'Jack Johnson')
-    post = user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    post.likes.create(user: user, post: post)
-    visit user_posts_path(user.id)
-
-    expect(page).to have_content('Likes: 1')
-  end
-
   scenario 'When I click on a post, it redirects me to that post\'s show page' do
-    user = User.create(name: 'Jack Johnson')
-    post = user.posts.create(user: user, title: 'My Post', content: 'This is my post content')
-    visit user_posts_path(user.id)
+    visit user_posts_path(@user)
     click_link 'My Post'
 
-    expect(current_path).to eq(user_post_path(user, post))
+    expect(current_path).to eq(user_post_path(@user, @post))
   end
 end
--- spec/views/show_spec.rb
+++ spec/views/show_spec.rb
@@ -1,61 +1,34 @@
 require 'rails_helper'
 
 RSpec.feature 'User Profile and Posts' do
-  scenario 'I can see the user\'s profile picture' do
-    user = User.create(name: 'John Doe')
+  let(:user) { User.create(name: 'John Doe', bio: 'I love coding!', post_count: 5) }
 
+  before do
     visit user_path(user)
+  end
 
+  scenario 'I can see the user\'s profile picture' do
     expect(page).to have_css('.userImg')
   end
 
   scenario 'I can see the user\'s username' do
-    user = User.create(name: 'Jack Johnson')
-
-    visit user_path(user)
-
-    expect(page).to have_content('Jack Johnson')
+    expect(page).to have_content(user.name)
   end
 
   scenario 'I can see the number of posts the user has written' do
-    user = User.create(name: 'John Doe', post_count: 5)
-
-    visit user_path(user)
-
-    expect(page).to have_content('Number of posts: 5')
+    expect(page).to have_content("Number of posts: #{user.post_count}")
   end
 
   scenario 'I can see the user\'s bio' do
-    user = User.create(name: 'John Doe', bio: 'I love coding!')
-
-    visit user_path(user)
-
-    expect(page).to have_content('I love coding!')
-  end
-
-  scenario 'I can see the user\'s first 3 posts' do
-    user = User.create(name: 'John Doe')
-    user.posts.create(user: user, title: 'My Post')
-
-    visit user_path(user)
-
-    expect(page).to have_css('user-post', count: 0)
+    expect(page).to have_content(user.bio)
   end
 
   scenario 'I can see a button to view all of a user\'s posts' do
-    user = User.create(name: 'John Doe')
-
-    visit user_path(user)
-
     expect(page).to have_link('All Posts', href: user_posts_path(user))
   end
 
   scenario 'When I click to see all posts, it redirects me to the user\'s post index page' do
-    user = User.create(name: 'John Doe')
-
-    visit user_path(user)
     click_link 'All Posts'
-
     expect(current_path).to eq(user_posts_path(user))
   end
 end
