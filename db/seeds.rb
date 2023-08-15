3edb716606a92746cbfe11fc8963ad3d9437c633
Cristian Zambrano <criszamcham@gmail.com>
2023-08-15 09:32:22 -0400

    Add letter opened and update confirmation

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-user1 = User.create!(name: "Jack Hughes", post_count: 0)
-post1 = user1.posts.create!(title: "My first post", content: "This is my first post")
-comment1 = post1.comments.create!(user: user1, post: post1, content: "This is my first comment")
