305ed9af5c76d9365e894df6a820549374377ca1
Cristian Zambrano <criszamcham@gmail.com>
2023-08-15 16:46:06 -0400

    Add comment count rspec and model method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,22 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :comments
+  has_many :likes
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :content, presence: true, length: { maximum: 1000 }
   validates :comment_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :like_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_create :increment_user_post_count
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def increment_user_post_count
+    user.increment!(:post_count)
+  end
 end
