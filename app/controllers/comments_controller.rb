842f869240d3cb9cabdea73b3621f229f9efd8cb
Cristian Zambrano <criszamcham@gmail.com>
2023-08-14 21:19:23 -0400

    Add post button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,25 @@
 class CommentsController < ApplicationController
-  def index; end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = @current_user.id
 
-  def show; end
+    if @comment.save
+      redirect_to user_post_path(@post.user, @post), notice: 'Comment was successfully added.'
+    else
+      redirect_to user_post_path(@post.user, @post), alert: 'Failed to add the comment.'
+    end
+  end
 
-  def edit; end
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @post, notice: 'Comment was successfully deleted.'
+  end
 
-  def new; end
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :posts
+      resources :comments, only: [:create]
   end
 end
